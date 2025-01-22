5958971e4fffcf43dbb6ba451f529157ddb4e24f
Heitor Teixeira <112182525+heitorctm@users.noreply.github.com>
2025-01-22 16:16:40 -0300

    sdfgvsd

--- auxiliar/tabela_processamentos.py
+++ auxiliar/tabela_processamentos.py
@@ -33,7 +33,7 @@ def t_tabela_processamentos(dados):
     ]
     
     dados.columns = novos_nomes
-
+    dados = remover_hifen(dados, ["cod_aai"])
     nova_ordem = [
         "cod_xp",
         "cod_aai",
@@ -54,7 +54,7 @@ def t_tabela_processamentos(dados):
         dados, colunas_not_varchar=["data_solicitacao"]
     )
 
-    dados = remover_hifen(dados, ["cod_aai"])
+    
 
 
     return dados
