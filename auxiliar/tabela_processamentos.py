7ec48ed858a8b3d42af7625367500cc98aaa064a
Heitor Teixeira <112182525+heitorctm@users.noreply.github.com>
2025-01-22 12:30:13 -0300

    teste

--- auxiliar/tabela_processamentos.py
+++ auxiliar/tabela_processamentos.py
@@ -14,6 +14,8 @@ def t_tabela_processamentos(dados):
     :return: DataFrame transformado.
     """
     # Remove as colunas indesejadas
+    print(1)
+    print(dados.head(1))
     colunas_a_remover = [
         "Nome Assessor Origem",
         "Nome Assessor Destino",
@@ -21,7 +23,8 @@ def t_tabela_processamentos(dados):
         "Código Solicitação",
     ]
     dados = dados.drop(columns=colunas_a_remover)
-
+    print(2)
+    print(dados.head(1))
     # Renomeia as colunas
     novos_nomes = [
         "cod_xp",
@@ -32,20 +35,27 @@ def t_tabela_processamentos(dados):
         "status",
     ]
     dados.columns = novos_nomes
+    print(3)
+    print(dados.head(1))
 
     # Formata as colunas de data
     dados = formatar_colunas_data_transf(
         dados, colunas_not_varchar=["data_solicitacao"]
     )
+    print(4)
+    print(dados.head(1))
     dados = formatar_colunas_data_transf(
         dados, colunas_not_varchar=["data_transferencia"]
     )
+    print(5)
     print(dados.head(1))
 
     # Remove hífens e adiciona aspas duplas
     dados = remover_hifen(dados, ["cod_aai", "cod_aai_destinho"])
+    print(6)
     print(dados.head(1))
     dados = adicionando_aspas_duplas(dados, ["none"])
+    print(7)
     print(dados.head(1))
     return dados
 
