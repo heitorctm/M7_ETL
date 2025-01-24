0f3bc1512ea10adbf33299b560fee14a664249ae
Heitor Teixeira <112182525+heitorctm@users.noreply.github.com>
2025-01-24 09:28:41 -0300

    sdfsdf

--- auxiliar/corretagem.py
+++ auxiliar/corretagem.py
@@ -31,11 +31,11 @@ def t_corretagem(dados):
     dados = dados[nova_ordem_colunas]
 
     # Aplica as transformações
-    dados = adicionando_aspas_duplas(dados, colunas_not_varchar=["data_ref", "Data"])
     dados = remover_linhas_sem_data(dados)
     dados = remover_letras_coluna(dados, coluna="Cod A")
+    dados = formatar_colunas_data(dados, colunas_not_varchar=["data_ref", "Data"])
     dados = truncar_2_casas(dados, colunas=["BOV", "Total"])
-    
+    dados = adicionando_aspas_duplas(dados, colunas_not_varchar=["data_ref"])
 
     return dados
 
